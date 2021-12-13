f69ea82be4a189687d38420786d51a192b2b4183
feature/damhobae <d.mo.b@baedamhoui-MacBookAir.local>
2021-12-13 23:52:41 +0900

    React Randign Page Add

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,45 +1,45 @@
-import React, { useEffect, useState } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import { AppBar, IconButton, Toolbar, Collapse } from '@material-ui/core';
-import SortIcon from '@material-ui/icons/Sort';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import { Link as Scroll } from 'react-scroll';
+import React, { useEffect, useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { AppBar, IconButton, Toolbar, Collapse } from "@material-ui/core";
+import SortIcon from "@material-ui/icons/Sort";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import { Link as Scroll } from "react-scroll";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    height: '100vh',
-    fontFamily: 'Nunito',
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100vh",
+    fontFamily: "Nunito",
   },
   appbar: {
-    background: 'none',
+    background: "none",
   },
   appbarWrapper: {
-    width: '80%',
-    margin: '0 auto',
+    width: "80%",
+    margin: "0 auto",
   },
   appbarTitle: {
-    flexGrow: '1',
+    flexGrow: "1",
   },
   icon: {
-    color: '#fff',
-    fontSize: '2rem',
+    color: "#fff",
+    fontSize: "2rem",
   },
   colorText: {
-    color: '#5AFF3D',
+    color: "#5AFF3D",
   },
   container: {
-    textAlign: 'center',
+    textAlign: "center",
   },
   title: {
-    color: '#fff',
-    fontSize: '4.5rem',
+    color: "#fff",
+    fontSize: "4.5rem",
   },
   goDown: {
-    color: '#5AFF3D',
-    fontSize: '4rem',
+    color: "#5AFF3D",
+    fontSize: "4rem",
   },
 }));
 export default function Header() {
--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,31 +1,31 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
 
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Typography from '@material-ui/core/Typography';
-import { Collapse } from '@material-ui/core';
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Typography from "@material-ui/core/Typography";
+import { Collapse } from "@material-ui/core";
 
 const useStyles = makeStyles({
   root: {
     maxWidth: 645,
-    background: 'rgba(0,0,0,0.5)',
-    margin: '20px',
+    background: "rgba(0,0,0,0.5)",
+    margin: "20px",
   },
   media: {
     height: 440,
   },
   title: {
-    fontFamily: 'Nunito',
-    fontWeight: 'bold',
-    fontSize: '2rem',
-    color: '#fff',
+    fontFamily: "Nunito",
+    fontWeight: "bold",
+    fontSize: "2rem",
+    color: "#fff",
   },
   desc: {
-    fontFamily: 'Nunito',
-    fontSize: '1.1rem',
-    color: '#ddd',
+    fontFamily: "Nunito",
+    fontSize: "1.1rem",
+    color: "#ddd",
   },
 });
 
